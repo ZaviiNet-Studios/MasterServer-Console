8821980654fb43cc827513c0c8e3d6aade4db62e
Trevin Pointer <gtextreme169@gmail.com>
2023-01-21 14:00:27 -0600

    Added Migration

--- ServerCommander.Data/Migrations/20230121195958_Deleted.Designer.cs
+++ ServerCommander.Data/Migrations/20230121195958_Deleted.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ServerCommander.Data;
 
@@ -10,9 +11,11 @@ using ServerCommander.Data;
 namespace ServerCommander.Data.Migrations
 {
     [DbContext(typeof(ServerCommanderContext))]
-    partial class ServerCommanderContextModelSnapshot : ModelSnapshot
+    [Migration("20230121195958_Deleted")]
+    partial class Deleted
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.2");
---
+++ ServerCommander.Data/Migrations/20230121195958_Deleted.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServerCommander.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Deleted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateDeleted",
+                table: "ServerInstances",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "FromServer",
+                table: "PlayerCountUpdates",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateDeleted",
+                table: "ServerInstances");
+
+            migrationBuilder.DropColumn(
+                name: "FromServer",
+                table: "PlayerCountUpdates");
+        }
+    }
+}
